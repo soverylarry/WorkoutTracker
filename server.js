01206b1d790b10fb83a091d708ed23eb6bc520ab
larry laconi <soverylarry@gmail.com>
2020-09-15 00:18:52 -0400

    adding server.js updates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
-require('dotenv').config()
+//require('dotenv').config()
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
